67c78a3ee992676e717120806d4a6dee430cbfc0
TomZab <16670@student.pb.edu.pl>
2026-03-14 10:22:54 +0100

    Interfejs: layout w XAML, główne widoki (lista + formularz), statyczne dane testowe. + crud
    .

---
+++ książka.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KatalogKsiazek
+{
+    public class Ksiazka
+    {
+        public int Id { get; set; }
+        public string Tytul { get; set; } = "";
+        public string Autor { get; set; } = "";
+        public int Rok { get; set; }
+        public string Gatunek { get; set; } = "";
+        public bool Przeczytana { get; set; }
+        public string Uwagi { get; set; } = "";
+    }
+}
